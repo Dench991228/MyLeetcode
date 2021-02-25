9df78b935ac73f3588cdb0499fe3edb522eb77d8
Dench991228 <denghanzhi85139@163.com>
2021-02-25 08:49:40 +0800

    完成了867题：矩阵转置

---
+++ src/Question867.java
@@ -0,0 +1,13 @@
+public class Question867 {
+    public int[][] transpose(int[][] matrix) {
+        int height = matrix.length;
+        int width = matrix[0].length;
+        int[][] result = new int[width][height];
+        for(int i=0;i<height;i++){
+            for(int j=0;j<width;j++){
+                result[j][i] = matrix[i][j];
+            }
+        }
+        return result;
+    }
+}
